7fc59d4606a6aea70ab2c5c3a962e9195af53679
dumitrubogdanmihai <bogdan_dumitru@sync.ro>
2019-11-22 08:51:54 +0200

    WA-3558: Configured table fragment to be insered.

--- frameworks/legaldocml/web/html-table-support.js
+++ frameworks/legaldocml/web/html-table-support.js
@@ -1,6 +1,4 @@
-
 sync.table.HtmlExtension = sync.table.HtmlExtension || {};
-
 
 /**
  * Constructor for the Extension.
@@ -19,8 +17,7 @@ goog.inherits(sync.table.HtmlExtension.Extension, sync.ext.Extension);
  */
 sync.table.HtmlExtension.Extension.prototype.editorCreated = function(editor) {
   goog.events.listen(editor, sync.api.Editor.EventTypes.ACTIONS_LOADED, function(e) {
-	  
-	
+
     var actionsManager = editor.getActionsManager();
 
     // Wrap the Insert Web Link action.
@@ -36,27 +33,33 @@ sync.table.HtmlExtension.Extension.prototype.editorCreated = function(editor) {
       insertWebLinkAction.getParams = function() {
         var params = sync.actions.InsertWebLink.prototype.getParams.call(this);
         return {
-          fragment: '<a href="' + params['fragment-url'] + '" xmlns="http://www.w3.org/1999/xhtml">' + 
-            params['fragment-url'] + 
-          '</a>'
+          fragment: '<a href="' + params['fragment-url'] + '" xmlns="http://www.w3.org/1999/xhtml">' +
+            params['fragment-url'] +
+            '</a>'
         }
       };
       actionsManager.registerAction(insertWebLinkActionId, insertWebLinkAction);
     }
-	
+
     var originalInsertTableAction = actionsManager.getActionById('insert.table');
     if (originalInsertTableAction) {
       var insertTableAction = new sync.actions.AkomanNtosoInsertTable(
         originalInsertTableAction,
         "ro.sync.ecss.extensions.commons.operations.InsertFragmentOperation",
         editor,
-        [sync.actions.InsertTable.TableTypes.HTML],
-        [sync.actions.InsertTable.ColumnWidthTypes.PROPORTIONAL,
+        [
+          sync.actions.InsertTable.TableTypes.HTML
+        ],
+        [
+          sync.actions.InsertTable.ColumnWidthTypes.PROPORTIONAL,
           sync.actions.InsertTable.ColumnWidthTypes.DYNAMIC,
-          sync.actions.InsertTable.ColumnWidthTypes.FIXED], 'Caption');
+          sync.actions.InsertTable.ColumnWidthTypes.FIXED
+        ],
+        'Caption');
+      // Override action defined in Document Type Association with one that ask for table caption.
       actionsManager.registerAction('insert.table', insertTableAction);
     }
-	
+
 
     addOldStyleTableActions(e.actionsConfiguration, editor);
   }, true);
@@ -86,8 +89,8 @@ function addOldStyleTableActions(actionsConfiguration, editor) {
       {"id": "insert.table.column.after", "type": "action"},
       {"id": "delete.table.column", "type": "action"}
     ];
-	
-     // Make table-related actions context-aware.
+
+    // Make table-related actions context-aware.
     [].concat(join_action, split_action, row_actions, column_actions).forEach(function(action) {
       sync.actions.TableAction.wrapTableAction(editor, action.id);
     });
@@ -139,58 +142,47 @@ function indexOfId(items, id) {
  */
 function isFrameworkActions(actionsConfiguration) {
   var toolbars = actionsConfiguration.toolbars;
-  
-  for( i = 0 ; i < toolbars.length; i++){ 
-	if(toolbars[i].name != "Builtin" && toolbars[i].name != "Review")
-	{
-		return true;
-	}
-	
+  for(var i = 0 ; i < toolbars.length; i++){
+    if(toolbars[i].name !== "Builtin" && toolbars[i].name !== "Review") {
+      return true;
+    }
   }
-
+  return false;
 }
 
 // Publish the extension.
 sync.ext.Registry.extension = new sync.table.HtmlExtension.Extension();
 
 sync.actions.AkomanNtosoInsertTable = function(a, b, c, d, e, f, g) {
-	 sync.actions.InsertTable.call(this, a, b, c, d, e, f, g);
-	 this.editor = c;
-}
+  sync.actions.InsertTable.call(this, a, b, c, d, e, f, g);
+  this.editor = c;
+};
 goog.inherits(sync.actions.AkomanNtosoInsertTable, sync.actions.InsertTable);
 
-sync.actions.AkomanNtosoInsertTable.prototype.invokeOperation = function(a, b, c) {
-	var caption = b;
+sync.actions.AkomanNtosoInsertTable.prototype.invokeOperation = function(tableType, opt_title, opt_callback) {
+  if(opt_title && opt_title.includes('<')) {
+    opt_title = opt_title.split("<").join("&lt;");
+  }
+  if(opt_title && opt_title.includes('>')) {
+    opt_title = opt_title.split(">").join("&gt;");
+  }
+  if(opt_title && opt_title.includes('&')) {
+    opt_title = opt_title.split("&").join("&amp;");
+  }
 
-	if(caption && caption.includes('<'))
-	{
-		caption = caption.split("<").join("&lt;");
-	}
-	if(caption && caption.includes('>'))
-	{
-		caption = caption.split(">").join("&gt;");
-	}
-	if(caption && caption.includes('&'))
-	{
-		caption = caption.split("&").join("&amp;");
-	}
-	
-		if (caption) {
-			this.editor.getActionsManager().invokeOperation(
-			"ro.sync.ecss.extensions.commons.operations.InsertFragmentOperation", 
-			{
-				fragment: "<table xmlns=\"http://docs.oasis-open.org/legaldocml/ns/akn/3.0/WD17\"><caption>" + caption + "</caption><tr><td><p></p></td></tr><tr><td><p></p></td></tr></table>"
-			}
-			);
-		}else {
-			this.editor.getActionsManager().invokeOperation(
-			"ro.sync.ecss.extensions.commons.operations.InsertFragmentOperation", 
-			{
-				fragment: "<table xmlns=\"http://docs.oasis-open.org/legaldocml/ns/akn/3.0/WD17\"><tr><td><p></p></td></tr><tr><td><p></p></td></tr></table>"
-			}
-			);
-		}
-}
-
-
-
+  if (opt_title) {
+    this.editor.getActionsManager().invokeOperation(
+      "ro.sync.ecss.extensions.commons.operations.InsertFragmentOperation",
+      {
+        fragment: "<table xmlns=\"http://docs.oasis-open.org/legaldocml/ns/akn/3.0/WD17\"><caption>" + opt_title + "</caption><tr><td><p></p></td></tr><tr><td><p></p></td></tr></table>"
+      }
+    );
+  }else {
+    this.editor.getActionsManager().invokeOperation(
+      "ro.sync.ecss.extensions.commons.operations.InsertFragmentOperation",
+      {
+        fragment: "<table xmlns=\"http://docs.oasis-open.org/legaldocml/ns/akn/3.0/WD17\"><tr><td><p></p></td></tr><tr><td><p></p></td></tr></table>"
+      }
+    );
+  }
+};
